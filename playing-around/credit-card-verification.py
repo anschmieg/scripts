79e6d1e75e6d13ab8f7f09c8bca87d3b14598e24
schmiega <6830368+schmiega@users.noreply.github.com>
2024-03-07 11:08:48 +0100

    minor changes

--- playing-around/credit-card-verification.py
+++ playing-around/credit-card-verification.py
@@ -1,19 +1,16 @@
 import argparse
 
+def calculate_luhn(number):
+    digits = list(map(int, str(number)))
+    odd_sum = sum(digits[-1::-2])
+    even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
+    return (odd_sum + even_sum)
 
 def calculate_verification_digit(number):
-    digits = list(map(int, str(number)))
-    odd_sum = sum(digits[-1::-2])
-    even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
-    return (10 - ((odd_sum + even_sum) % 10)) % 10
-
+    return (10 - (calculate_luhn(number) % 10)) % 10
 
 def luhn_check(number):
-    digits = list(map(int, str(number)))
-    odd_sum = sum(digits[-1::-2])
-    even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
-    return (odd_sum + even_sum) % 10 == 0
-
+    return calculate_luhn(number) % 10 == 0
 
 parser = argparse.ArgumentParser(
     description="This script calculates the verification digit for an input number and performs the Luhn verification."
@@ -30,7 +27,7 @@ number = input("Enter a number: ")
 
 if args.calculate:
     verification_digit = calculate_verification_digit(number)
-    print(f"The verification digit is {verification_digit}. Appending to input number.")
+    print(f"The verification digit is {verification_digit}.")
     number = str(number) + str(verification_digit)
 
 if luhn_check(number):
